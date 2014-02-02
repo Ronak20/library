b9947e1de87f4ae22e38bc7a27913beb5fda7736
Ronak <ronakbchaudhari@gmail.com>
2014-02-02 06:09:55 -0500

    TC24 added

--- src/main/test/com/library/test/http/TC24.java
+++ src/main/test/com/library/test/http/TC24.java
@@ -1,16 +1,15 @@
 package com.library.test.http;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.Arrays;
 import java.util.Collection;
 
+import junit.framework.TestCase;
+
 import org.apache.log4j.Logger;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import com.library.config.Constant;
@@ -19,18 +18,15 @@ import com.meterware.httpunit.SubmitButton;
 import com.meterware.httpunit.TableCell;
 import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebForm;
-import com.meterware.httpunit.WebLink;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
 import com.meterware.httpunit.WebTable;
 
-@RunWith(Parameterized.class)
-public class TC5 {
-	private static Logger logger = Logger.getLogger(TC5.class);
-	private String isbn;
+public class TC24 extends TestCase{
+	private static Logger logger = Logger.getLogger(TC24.class);
 
-	public TC5(String isbn) {
-		this.isbn = isbn;
+	public TC24(String s) {
+		super(s);
 	}
 
 	@Parameters
@@ -60,21 +56,23 @@ public class TC5 {
 	}
 
 	@Test
-	public void testTC5AddCopiesOfTwoTitle() throws Exception {
-		logger.info("Entered testTC5AddCopiesOfTwoTitle");
+	public void testTC24RemoveTitle() throws Exception {
+		logger.info("Entered testTC24RemoveTitle");
 		WebConversation conversation = new WebConversation();
 		WebRequest requestAdd = new GetMethodWebRequest(Constant.BOOK_ADD_URL);
 		WebResponse responseAdd = conversation.getResponse(requestAdd);
 		WebForm addBookForm = responseAdd.getFormWithID("addBookForm");
-		logger.debug("Add Book Form : \n" + responseAdd.getText());
 		addBookForm.setParameter("bookname",
 				"Mybook" + System.currentTimeMillis());
 		addBookForm.setParameter("copies",
 				"" + (int) (System.currentTimeMillis()));
+		String isbn = "testisbn" + (int) (System.currentTimeMillis());
 		addBookForm.setParameter("isbn", isbn);
-		/*SubmitButton addBookSubmitButton = addBookForm
-				.getSubmitButton("addBookSubmit");
-		addBookForm.submit(addBookSubmitButton);*/
+		/*
+		 * SubmitButton addBookSubmitButton = addBookForm
+		 * .getSubmitButton("addBookSubmit");
+		 * addBookForm.submit(addBookSubmitButton);
+		 */
 		addBookForm.submit();
 
 		logger.info("One book added");
@@ -85,34 +83,18 @@ public class TC5 {
 		WebTable bookListTable = response3.getTableWithID("bookListTable");
 		TableCell tableCell = bookListTable.getTableCell(
 				bookListTable.getRowCount() - 1, 0);
-		String bookidUpdate = tableCell.getText();
-		logger.debug("bookidUpdate : " + bookidUpdate);
-		TableCell tableUpdateCell = bookListTable.getTableCell(
+		String bookidDelete = tableCell.getText();
+		logger.debug("bookidDelete : " + bookidDelete);
+		TableCell tableDeleteCell = bookListTable.getTableCell(
 				bookListTable.getRowCount() - 1,
-				bookListTable.getColumnCount() - 2);
-		TableCell tableBooknameCell = bookListTable.getTableCell(
-				bookListTable.getRowCount() - 1,
-				bookListTable.getColumnCount() - 4);
-		logger.debug("Bookname : " + tableBooknameCell.getText());
+				bookListTable.getColumnCount() - 1);
 
-		logger.debug(tableUpdateCell.getText());
-		// clicking update link in table
-		WebLink updateLink = tableUpdateCell.getLinks()[0];
-		WebResponse updateClickResponse = updateLink.click();
-		WebForm editBookForm = updateClickResponse
-				.getFormWithID("editBookForm");
-		editBookForm.setParameter("bookid", bookidUpdate);
-		editBookForm.setParameter("bookname", tableBooknameCell.getText());
-		editBookForm.setParameter("isbn", isbn);
-		editBookForm.setParameter("copies", "" + 10);
-		SubmitButton updateBookSubmitButton = addBookForm
-				.getSubmitButton("updateBookSubmit");
-		editBookForm.submit(updateBookSubmitButton);
+		logger.debug(tableDeleteCell.getText());
 
-		// checking whether book is updated or not
-		logger.info("checking updated book");
+		// checking whether book is deleted or not
+		logger.info("checking deleted book");
 		WebRequest requestGetBook = new GetMethodWebRequest(
-				Constant.BOOK_UPDATE_URL+bookidUpdate);
+				Constant.BOOK_DELETE_URL+bookidDelete);
 		WebResponse responseGetBook = conversation.getResponse(requestGetBook);
 		WebTable bookListUpdatedTable = responseGetBook
 				.getTableWithID("bookListTable");
@@ -120,8 +102,8 @@ public class TC5 {
 				bookListUpdatedTable.getRowCount() - 1,
 				bookListUpdatedTable.getColumnCount() - 3);
 		logger.debug("Updated copies : " + tableUpdatedCell.getText());
-		assertEquals(10, Integer.parseInt(tableUpdatedCell.getText()));
+		Assert.assertNotSame(isbn, tableUpdatedCell.getText());
 
-		logger.info("Exited testTC5AddCopiesOfTwoTitle");
+		logger.info("Exited testTC24RemoveTitle");
 	}
 }
