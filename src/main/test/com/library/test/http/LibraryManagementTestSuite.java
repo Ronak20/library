a086a37e8de9792510dbc8aa8c2effb903e1c164
Ronak <ronakbchaudhari@gmail.com>
2014-02-07 21:18:13 -0500

    blank test cases added

---
+++ src/main/test/com/library/test/http/LibraryManagementTestSuite.java
@@ -0,0 +1,17 @@
+package com.library.test.http;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TC1.class, TC2.class, TC3.class, TC4.class, TC5.class,
+		TC6.class, TC7.class, TC8.class, T10.class, TC11.class, TC12.class,
+		TC13.class, TC14.class, TC15.class, TC16.class, TC17.class, TC18.class,
+		TC19.class, TC20.class, TC21.class, TC22.class, TC23.class, TC24.class,
+		TC25.class, TC25a.class, TC25b.class,
+
+})
+public class LibraryManagementTestSuite {
+
+}
---
+++ src/main/test/com/library/test/http/TC6.java
@@ -0,0 +1,24 @@
+package com.library.test.http;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TC6 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
